e9f01344778e31218358136b1c7119eb38a87dfe
lipengpeng <lipurpose@aliyun.com>
2021-03-07 18:54:01 +0800

    bubbleData 提交

---
+++ assets/Script/BubbleData.ts
@@ -0,0 +1,89 @@
+ class Bubble {
+     private color = 0;
+     constructor(color){
+         this.color = color;
+     }
+     getColor(){
+         return this.color;
+     }
+ }
+export default class BubbleData {
+    private bubbleNums = 0;
+    private bubbles = []; 
+    private bubbleLength = [];
+    
+    constructor() {
+        
+    }
+
+    init(bubbleData : [[number]]){
+        for(let i = 0; i < bubbleData.length; ++i){
+            this.bubbles[i] = [];
+            for(let j = 0; j < bubbleData[i].length; ++j){
+                this.bubbles[i][j] = new Bubble(bubbleData[i][j]);
+            }
+            this.bubbleLength[i] = bubbleData[i].length;
+        }
+    }
+    
+    getBubble(row, col){
+        if(this.bubbles[row]){
+            return this.bubbles[row][col];
+        }
+        return null;
+    }
+
+    addBubble(row, col, color){
+        this.bubbles[row] = this.bubbles[row] || [];
+        if(!this.bubbles[row][col]){
+            this.bubbleLength[row] += 1;
+        }
+        this.bubbles[row][col] = new Bubble(color);
+    }
+
+    deleteBubble(row, col){
+        if(this.getBubble(row, col)){
+            this.bubbles[row][col] = null;
+            this.bubbleLength[row] -= 1;
+        }
+        while(this.bubbleLength[row] == 0 && row == this.bubbles.length - 1){
+            this.bubbles.splice(row, 1);
+            this.bubbleLength.splice(row, 1);
+            row -= 1;
+        }
+    }
+    deleteNoLinkedBubbles(){
+        let deleteBubbles = {};
+        for(let i = 0; i < this.bubbles.length; ++i){
+            for(let j = 0; j < this.bubbles[i].length; ++j){
+                if(this.bubbles[i][j]){
+                    deleteBubbles["" + i + "_" + j] = true;
+                }
+            }
+        }
+        for(let i = 0; i < this.bubbles[1].length; ++i){
+            if(this.bubbles[0][i]){
+                this._markAsLinked(0, i, deleteBubbles);
+            }
+        }
+        return deleteBubbles;
+    }
+    _markAsLinked(row, col, noMarkedTable){
+        let key = "" + row + "_" + col
+        if(!noMarkedTable[key]){
+            return;
+        }
+        delete noMarkedTable[key];
+        //左边
+        this._markAsLinked(row, col - 1, noMarkedTable);
+        //右边
+        this._markAsLinked(row, col + 1, noMarkedTable);
+        //下边
+        let right = col;
+        if((row + 1) % 2 == 0){
+            right += 1;
+        }
+        this._markAsLinked(row + 1, right - 1, noMarkedTable);
+        this._markAsLinked(row + 1, right - 1, noMarkedTable);
+    }
+}
